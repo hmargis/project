4ecb1dccdbe74f1291d5bb962a498518069e796f
hmargis <hendrik.margis@googlemail.com>
2024-01-17 20:28:23 +0100

    new commit

--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["fail-svgrepo-com.svg","favicon.png","heart-alt-svgrepo-com.svg","note-medical-svgrepo-com.svg","pass-svgrepo-com.svg","spark-svgrepo-com.svg","trash-blank-svgrepo-com.svg","trash-can-svgrepo-com.svg","trash-can-with-cover-svgrepo-com.svg"]),
 	mimeTypes: {".svg":"image/svg+xml",".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.BDSEaNfU.js","app":"_app/immutable/entry/app.ul36uuYL.js","imports":["_app/immutable/entry/start.BDSEaNfU.js","_app/immutable/chunks/scheduler.ZIB-3X42.js","_app/immutable/chunks/singletons.D2BnVoqF.js","_app/immutable/chunks/index.-YLKjFAA.js","_app/immutable/entry/app.ul36uuYL.js","_app/immutable/chunks/scheduler.ZIB-3X42.js","_app/immutable/chunks/index.M2rPet9x.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.8eMaMy2Q.js","app":"_app/immutable/entry/app.R0YyE6tn.js","imports":["_app/immutable/entry/start.8eMaMy2Q.js","_app/immutable/chunks/scheduler.ZIB-3X42.js","_app/immutable/chunks/singletons.wgnWz9mo.js","_app/immutable/chunks/index.-YLKjFAA.js","_app/immutable/entry/app.R0YyE6tn.js","_app/immutable/chunks/scheduler.ZIB-3X42.js","_app/immutable/chunks/index.M2rPet9x.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js')),
