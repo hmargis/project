a16b00bc78868776f5f939ec9b96a79a1f45e4f3
hmargis <hendrik.margis@googlemail.com>
2024-01-17 20:33:10 +0100

    help

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.43XQSlT5.js","_app/immutable/chunks/scheduler.ZIB-3X42.js","_app/immutable/chunks/index.M2rPet9x.js","_app/immutable/chunks/singletons.wgnWz9mo.js","_app/immutable/chunks/index.-YLKjFAA.js"];
-export const stylesheets = [];
-export const fonts = [];
